1af34b372ccca74cb408c49402b2162246083d37
Egor284 <116432625+Egor284@users.noreply.github.com>
2023-01-07 14:48:37 +0700

    Update ValidData.py

--- ValidData.py
+++ ValidData.py
@@ -1,12 +1,12 @@
-validnumber='9829874215'
-validpassword='Exat2012'
-validmail='vakalov.92.20.12@gmail.com'
-validlogin='Тест'
-validlc='22125881254784125845'
-novalidnumber='9879541123'
-novalidmail= 'qwrqwsdfhsteyz@mail.com'
-novalidlogin= 'Тестик'
-novalidlc= '12412451426134631461'
-vname= 'Тест'
-vsname= 'Аккаунт'
+validnumber='9829874215' #валидный номер телефона
+validpassword='Exat2012' #валидный пароль
+validmail='vakalov.92.20.12@gmail.com' #валидный email
+validlogin='Тест' #валидный логин
+validlc='22125881254784125845' #валидный лицевой счёт
+novalidnumber='9879541123' #невалидный номер телефона
+novalidmail= 'qwrqwsdfhsteyz@mail.com' #невалидный email
+novalidlogin= 'Тестик' #невалидный логин
+novalidlc= '12412451426134631461' #невалидный лицевой счёт
+vname= 'Тест' #имя для регистрации
+vsname= 'Аккаунт' #фамилия для регистрации
 
